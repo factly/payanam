c4760474b197e95aaa0112aaaec8ddb228fea995
answerquest <nikhil.js@gmail.com>
2021-09-25 21:34:41 +0530

    fastpi setup

---
+++ api_sample.py
@@ -0,0 +1,18 @@
+from typing import Optional
+from payanam_launch import app
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+    # example: http://localhost:8000/items/3?q=hello
+
+
+@app.get("/ola")
+async def read_root():
+    return {"Ola": "Monde"}
+
---
+++ payanam_launch.py
@@ -0,0 +1,9 @@
+from typing import Optional
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# can add modules having api calls below
+
+import api_sample
+
